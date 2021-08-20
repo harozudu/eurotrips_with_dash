82b9a0f5c865528cc283794e39705f8c83024fb8
harozudu <65071575+harozudu@users.noreply.github.com>
2021-08-20 12:31:18 +0200

    commit

--- app.py
+++ app.py
@@ -29,6 +29,7 @@ Data is coming from:
 """
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+server = app.server
 
 ### ---------------- Importing and cleaning data -----------------------------
 
